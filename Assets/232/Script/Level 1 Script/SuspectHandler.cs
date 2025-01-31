6c6a69bdb20d74c11c7e511163463d0b6c22720a
Hyoju Yang <hyang34@gmu.edu>
2025-01-31 15:29:31 -0500

    단서하다말았음

--- Assets/232/Script/Level 1 Script/SuspectHandler.cs
+++ Assets/232/Script/Level 1 Script/SuspectHandler.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class SuspectHandler : MonoBehaviour
 {
-    public TextMeshProUGUI chancesText; //  ?? ??? ?? UI ???  / UI text for chances
-    public string correctSuspect = "A"; // ??? ??? / The correct suspect
+    public TextMeshProUGUI chancesText; // UI text for chances
+    public string correctSuspect = "A"; //The correct suspect
 
-    private int chances = 3; // ?? ? / total chances
+    private int chances = 3; //total chances
 
     private void Start()
     {
@@ -18,8 +19,8 @@ public class SuspectHandler : MonoBehaviour
 
     private void Update()
     {
-        // ??? ?? / detect mouse click
-        if (Input.GetMouseButtonDown(0)) // ?? ??? ??? 
+        //detect mouse click
+        if (Input.GetMouseButtonDown(0))
         {
             DetectSuspectClick();
         }
@@ -34,15 +35,15 @@ public class SuspectHandler : MonoBehaviour
             return; // Ignore clicks on UI elements like the pop-up panel or buttons
         }
 
-        // ????? ??? ??? ?? ??? ?? ?? / Cast a ray from the camera to the world point of the mouse position
+        //Cast a ray from the camera to the world point of the mouse position
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
 
         if (hit.collider != null)
         {
-            // ??? ????? ?? /Check if the clicked object is a suspect
-            string clickedSuspectName = hit.collider.gameObject.name; // ??? ???? ???? / Get the suspect's GameObject name
-            ChooseSuspect(clickedSuspectName); // ??? ??? ??? ChooseSuspect ???? ?? / Pass the name to the ChooseSuspect method
+            //Check if the clicked object is a suspect
+            string clickedSuspectName = hit.collider.gameObject.name; //Get the suspect's GameObject name
+            ChooseSuspect(clickedSuspectName); //Pass the name to the ChooseSuspect method
         }
     }
 
@@ -50,23 +51,27 @@ public class SuspectHandler : MonoBehaviour
     {
         if (suspectName == correctSuspect)
         {
-            // ?? ??? ????? / if correct suspect
+            //if correct suspect
             Debug.Log("Congratulation! going to next level...");
-            SceneManager.LoadScene("Narration2"); // ?? ? ????? going to next scene when getting correct suspect (next scene name)
+            SceneManager.LoadScene("Narration2"); // going to next scene when getting correct suspect (next scene name)
         }
         else
         {
-            // ?? ??? when player chose wrong suspect -1 chance
-            chances--; // -1 ?? 
-            UpdateChancesText(); // UI ??? / Update the UI
-            Debug.Log("?????! ?? ??: " + chances);
+            DecreaseChances();
+        }
+    }
 
-            if (chances <= 0)
-            {
-                // ??? 0??? when chances are all used 0/3
-                Debug.Log("Game Over! you got no chances left.");
-                SceneManager.LoadScene("LoseScene"); // ???? ?? ? going to Lose scene (name of next scene)
-            }
+    public void DecreaseChances()
+    {
+        //when player chose wrong suspect -1 chance
+        chances--;
+        UpdateChancesText(); //Update the UI
+        Debug.Log("?????! ?? ??: " + chances);
+
+        if (chances <= 0)
+        {
+            Debug.Log("Game Over! you got no chances left.");
+            SceneManager.LoadScene("LoseScene"); // going to Lose scene (name of next scene)
         }
     }
 
