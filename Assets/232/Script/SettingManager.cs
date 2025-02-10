b6cacfaf9072378f3130a12c926a1ca4001e716d
Hyoju Yang <hyang34@gmu.edu>
2025-02-09 21:27:21 -0500

    Settings/HowtoPlay panels improvements

--- Assets/232/Script/SettingManager.cs
+++ Assets/232/Script/SettingManager.cs
@@ -2,40 +2,36 @@ using UnityEngine;
 
 public class SettingManager : MonoBehaviour
 {
-    public GameObject settingsPanel;  // 설정 패널을 연결할 변수
-    private bool isGamePaused = false;  // 게임이 일시 정지 상태인지 확인하는 변수
+    public GameObject settingsPanel;
 
-    void Update()
+    private bool isGamePaused = false;
+
+    private void Update()
     {
-        // ESC 키를 눌렀을 때
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (isGamePaused)
+            if (settingsPanel.activeSelf)
             {
-                ResumeGame();  // 게임 재개
+                settingsPanel.SetActive(false);
+                UpdateGamePauseState();
             }
             else
             {
-                PauseGame();  // 게임 일시 정지
+                settingsPanel.SetActive(true);
+                UpdateGamePauseState();
             }
         }
     }
 
-    // 게임 일시 정지
-    void PauseGame()
+    private void UpdateGamePauseState()
     {
-        settingsPanel.SetActive(true);  // 설정 패널 활성화
-        Time.timeScale = 0f;  // 게임 일시 정지
-        isGamePaused = true;  // 게임이 일시 정지 상태로 변경
-    }
-
-    // 게임 재개
-    void ResumeGame()
-    {
-        settingsPanel.SetActive(false);  // 설정 패널 비활성화
-        Time.timeScale = 1f;  // 게임 진행
-        isGamePaused = false;  // 게임 재개 상태로 변경
+        if (settingsPanel.activeSelf)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
     }
 }
-
-
